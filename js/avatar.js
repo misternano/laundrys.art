39216fc12105f0b0a181c8a67bcfdb7c322ccc3f
misternano <cjstitt@icloud.com>
2022-12-12 21:06:43 -0500

    Uniformed names, added carousel for img alts, small screen fixes

--- js/avatar.js
+++ js/avatar.js
@@ -1,14 +1,14 @@
 // UPDATE AVATAR
 const setAvatar = () => {
-    fetch("https://api.lanyard.rest/v1/users/344973971638321162")
-        .then((response) => response.json())
-        .then((json) => {
-            const avatarHash = json.data.discord_user.avatar;
-            const url = `https://cdn.discordapp.com/avatars/344973971638321162/${avatarHash}.png`;
-            $('#avatar').attr("src", url);
-        });
+	fetch("https://api.lanyard.rest/v1/users/344973971638321162").then((response) => response.json()).then((json) => {
+		const avatarHash = json.data.discord_user.avatar;
+		const spotify = json.data.listening_to_spotify;
+		const url = `https://cdn.discordapp.com/avatars/344973971638321162/${avatarHash}.png`;
+		$("#avatar").attr("src", url);
+		if (spotify) {
+			$("#avatar").css("border-color", "#1fdf64");
+		}
+	});
 }
 
-$(document).ready(() => {
-    setAvatar();
-});
+$(document).ready(setAvatar());
